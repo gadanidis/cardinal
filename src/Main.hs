85f1557fc8c8590729f8fca05cba37dd33bc6081
Tim Gadanidis <tim@gadanidis.ca>
2020-01-08 18:18:18 -0500

    add basic interface

--- src/Main.hs
+++ src/Main.hs
@@ -3,21 +3,48 @@ module Main where
 import qualified Data.Map.Strict as M
 import           Data.Map.Strict (Map)
 
-main :: IO ()
-main = undefined
+import           Data.List.Split (splitOn)
+import           Text.ParserCombinators.ReadP
+import           Data.Char (isLetter)
+import           Text.Read (readMaybe)
+import           Data.Maybe (fromMaybe)
 
-ballots :: [Map String Vote]
-ballots = map M.fromList [ [("john", Oppose), ("meg", Support)]
-                         , [("john", Abstain), ("meg", Support)]
-                         , [("john", Support), ("meg", Support)]
-                         , [("john", Support), ("meg", Support)]
-                         , [("john", Oppose), ("meg", Support)]
-                         ]
+main :: IO ()
+main = do
+    putStr "enter input file path: "
+    inputFile <- getLine
+    input <- readFile inputFile
+    let ls = lines (rmSpace input)
+    let ballots = map parseBallot ls
+    print $ result ballots
+
+vote :: ReadP (String, Vote)
+vote = do
+    name <- many1 (satisfy isLetter)
+    _ <- many1 (satisfy (\x -> x == ':' || x == ' '))
+    response <- many1 (satisfy isLetter)
+    return (name, toVote response)
+
+rmSpace :: String -> String
+-- removes all spaces from a string
+rmSpace "" = ""
+rmSpace (c:cs) = if c == ' '
+                then rmSpace cs
+                else c : rmSpace cs
+
+parseVote :: String -> Map String Vote
+parseVote = M.fromList . map fst . readP_to_S vote
+
+parseBallot :: String -> Map String Vote
+parseBallot b = M.unions $ map parseVote (splitOn "," b)
 
 data Vote = Support
           | Oppose
           | Abstain
-          deriving (Eq, Show)
+          deriving (Eq, Show, Read)
+
+toVote :: String -> Vote
+toVote = fromMaybe Abstain . readMaybe
 
 vote2int :: Vote -> Int
 vote2int Support = 1
